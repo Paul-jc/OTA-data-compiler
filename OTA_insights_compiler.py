e7db3d41a67de34e1e5fcfc95bcd371e0a5d875a
Paul-jc <34979688+Paul-jc@users.noreply.github.com>
2018-08-20 06:22:23 +1000

    Update OTA_insights_compiler.py

--- OTA_insights_compiler.py
+++ OTA_insights_compiler.py
@@ -21,12 +21,15 @@
 #  MA 02110-1301, USA.
 
 """
-This program is used to compile a large number of files that were downloaded from the site OTAinsights (utilising seperate program) into a single CSV file
-for the purpose of running through a machine learning library. This is a subscriber only website that scrapes data from Online Travel Agent websites
-and combiles it daily. In order to obtain this data and in absence of an API a scraper was developed to dowload all files with pricing history for any given day.
+This program is used to compile a large number of files that were downloaded from the site OTAinsights
+(utilising seperate program) into a single CSV file for the purpose of running through a machine learning 
+library. This is a subscriber only website that scrapes data from Online Travel Agent websites and combiles it daily. 
+In order to obtain this data and in absence of an API a scraper was developed to dowload all files with pricing
+history for any given day.
 
-The format of the files had the date in poorly formatted form in cell A1. In order to make this easily computable for future purposes this program places
-the date beside each row of data, as well as week of year, day of week and month of year data for ease of future computing.
+The format of the files had the date in poorly formatted form in cell A1. In order to make this easily computable 
+for future purposes this program places the date beside each row of data, as well as week of year, day of week and 
+month of year data for ease of future computing.
 
 For use place the folder path as the file_folder variable
 """
